ae4d79542841bff8ff37a425172a1e7db1a266f2
Anandu B Ajith <reach2anandu@gmail.com>
2019-08-20 16:02:50 +0530

    lint

--- download-files.js
+++ download-files.js
@@ -1,20 +1,21 @@
-const fs = require('fs');
-const download = require('download');
-const rimraf = require('rimraf');
+const fs = require("fs");
+const download = require("download");
+const rimraf = require("rimraf");
 
+const fileNames = ["BTECH_dues.pdf", "PG_dues.pdf", "PHD_dues.pdf"];
 
-const fileNames = ['BTECH_dues.pdf', 'PG_dues.pdf', 'PHD_dues.pdf'];
+rimraf("./PDFs/*", function() {
+  console.log("Cleaning up.");
+});
 
-rimraf('./PDFs/*', function () { console.log("Cleaning up.") });
-
-Promise
-	.all(
-		fileNames
-			.map(fileName => download(`http://nitc.ac.in/app/webroot/img/upload/${fileName}`, 'PDFs'))
-	)
-	.then(() => {
-	    console.log('Downloaded');
-	})
-	.catch((e) => {
-		console.error(e);
-	})
+Promise.all(
+  fileNames.map(fileName =>
+    download(`http://nitc.ac.in/app/webroot/img/upload/${fileName}`, "PDFs")
+  )
+)
+  .then(() => {
+    console.log("Downloaded");
+  })
+  .catch(e => {
+    console.error(e);
+  });
--- update-db.js
+++ update-db.js
@@ -3,52 +3,61 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../service-key.json");
 
 function Extract(file) {
-    return new Promise(function(resolve, reject) {
-         pdf_table_extractor(file, resolve, reject);
-    });
+  return new Promise(function(resolve, reject) {
+    pdf_table_extractor(file, resolve, reject);
+  });
 }
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://nitc-hostel-dues.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://nitc-hostel-dues.firebaseio.com"
 });
 const db = admin.database();
 
 function setLastUpdated(s) {
-	let date = s.split(' ').reverse().slice(0, 3).reverse().join(' ').replace(')', '').toUpperCase();
-	return db.ref('last_updated').set(date);
+  let date = s
+    .split(" ")
+    .reverse()
+    .slice(0, 3)
+    .reverse()
+    .join(" ")
+    .replace(")", "")
+    .toUpperCase();
+  return db.ref("last_updated").set(date);
 }
 
 function parsePDF(result) {
-	let promises = [];
-	setLastUpdated(result.pageTables[0].tables[0][0]);
-	// use filter and flatMaps?
-	result.pageTables.map(page => {
-		page.tables.map(item => {
-			if (item[0].length === 9) {
-				promises.push(
-					db.ref(item[0]).set({
-						name: item[1],
-						due: item[2]
-					})
-				);
-			}
-		})
-	});
-	return Promise.all(promises);
+  let promises = [];
+  setLastUpdated(result.pageTables[0].tables[0][0]);
+  // use filter and flatMaps?
+  result.pageTables.map(page => {
+    page.tables.map(item => {
+      if (item[0].length === 9) {
+        promises.push(
+          db.ref(item[0]).set({
+            name: item[1],
+            due: item[2]
+          })
+        );
+      }
+    });
+  });
+  return Promise.all(promises);
 }
 
-const fileNames = ['PDFs/BTECH_dues.pdf', 'PDFs/PG_dues.pdf', 'PDFs/PHD_dues.pdf'];
-
+const fileNames = [
+  "PDFs/BTECH_dues.pdf",
+  "PDFs/PG_dues.pdf",
+  "PDFs/PHD_dues.pdf"
+];
 
 filePromises = fileNames.map(file => Extract(file).then(parsePDF));
 
-Promise
-	.all(filePromises)
-	.then(() => {
-		console.log("Successfully updated");
-		process.exit(0);
-	})
-	.catch((e) => {
-		console.log("Error", e)
-	});
+Promise.all(filePromises)
+  .then(() => {
+    console.log("Successfully updated");
+    process.exit(0);
+  })
+  .catch(e => {
+    console.log("Error", e);
+  });
