ad8d044864e68de2b9c015d8852c0f5e0cadae51
Anandu B Ajith <reach2anandu@gmail.com>
2020-01-23 13:22:20 +0530

    add auto check script to send notification

---
+++ main.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+function getLastUpdated() {
+    return axios
+      .get("https://nitc-hostel-dues.firebaseio.com/last_updated.json")
+      .then(response => response.data);
+}
+
+async function updateDatabase() {
+    await exec('node update-db.js');
+};
+async function sendNotification() {
+    await exec('node notify-users.js');
+};
+
+
+async function update() {
+    var oldDate = await getLastUpdated();
+    updateDatabase();
+    var newDate = await getLastUpdated();
+    if ( oldDate != newDate ) {
+        sendNotification();
+        console.log("Notification sent with ", newDate)
+    } else {
+        console.log("No new data : ", newDate);
+    }
+    process.exit(0);
+}
+update();
